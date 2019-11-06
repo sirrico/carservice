5b9d25cf4de19f0328a8b9fc48d6d9900b3092ef
sirrico <tristansirrico@gmail.com>
2019-11-05 21:52:12 -0500

    Adding database function with SQLite3

--- main.cpp
+++ main.cpp
@@ -2,14 +2,24 @@
 #include <iomanip>
 #include <fstream>
 #include <string>
-#include <cstddef>        
+#include <cstddef>      
+#include <stdio.h>
+#include <sqlite3.h>   
 
 using namespace std;
 
-int main() {
-        
-        
-	
-    	return 0;
-}
+int main(int argc, char* argv[]) {
+   sqlite3 *db;
+   char *zErrMsg = 0;
+   int rc;
 
+   rc = sqlite3_open("test.db", &db);
+
+   if( rc ) {
+      fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+      return(0);
+   } else {
+      fprintf(stderr, "Opened database successfully\n");
+   }
+   sqlite3_close(db);
+}
